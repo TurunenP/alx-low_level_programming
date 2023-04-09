54a64671152b4b8e455f9a5340c877986340f075
Purity Turunen <k7jepu00@students.oamk.fi>
2023-04-09 04:59:31 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>	
+#include "main.h"
+
+/**	
+ * main - prints the name of the program	
+ * @argc: number of arguments		
+ * @argv: array of arguments
+ * 
+ * Return: Always 0 (Success)		
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+	
+}
