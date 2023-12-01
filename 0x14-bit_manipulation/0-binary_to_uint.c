70f22f1e42e8212d0795e53ad895150fcc630e4c
TurunenP <k7jepu00@students.oamk.fi>
2023-12-01 01:47:55 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-
 /**
- * binary_to_uint
- * @b: string for binary number
- * Return: number converted
+ * binary_to_uint - Converts a binary string to an unsigned int.
+ * @b: A string representing a binary number.
+ *
+ * Return: The decimal value of the binary number, or 0 if invalid.
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -12,15 +12,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int dec_val = 0;
 
 	if (!b)
-		return (0);
+	return (0);
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
+	if (b[i] < '0' || b[i] > '1')
 
+		return (0);
+
+	dec_val = 2 * dec_val + (b[i] - '0');
+	}
 	return (dec_val);
 }
-
